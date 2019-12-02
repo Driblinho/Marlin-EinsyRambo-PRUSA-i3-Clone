684ef5394a4ae2a061a77d6a3ff29642bcea12a9
Driblinho <irekkuliga@gmail.com>
2019-12-02 16:43:29 +0100

    fix NOZZLE_TO_PROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -924,7 +924,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 28, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 21, 0, 0 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 40
